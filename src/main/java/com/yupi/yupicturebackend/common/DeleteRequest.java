2f759e085cd7e16a54ad5bc25dc57a1798570432
MilesMirak <git@gitee.com:elysianrealm02/yu-picture.git>
2025-03-17 21:39:30 +0800

    智能协调云图库项目初始化

---
+++ src/main/java/com/yupi/yupicturebackend/common/DeleteRequest.java
@@ -0,0 +1,15 @@
+package com.yupi.yupicturebackend.common;
+
+import lombok.Data;
+
+/**
+ * 通用的删除请求类
+ */
+@Data
+public class DeleteRequest {
+    /**
+    * id
+    */
+    private long id;
+    private static final long serialVersionUID = 1L;
+}
